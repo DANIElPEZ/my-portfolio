9057f34d3bdd4f57172559319ebfd9ec27d30e22
Daniel Gonzalez <daniel.gonzalezubaque@gmail.com>
2025-02-09 18:25:39 -0500

    finalizing the portfolio

--- src/views/projects/mobile.jsx
+++ src/views/projects/mobile.jsx
@@ -1,10 +1,36 @@
 import { Nav_bar } from "./../../components/nav_bar.jsx";
 import { Footer } from "./../../components/footer.jsx";
+import { Project } from "../../components/project_section.jsx";
+import iconic1 from "./../../assets/iconic1.png";
+import iconic2 from "./../../assets/iconic2.png";
+/* mobile tecnologies */
+import flutter from "./../../assets/flutter.webp";
+/* Files *.apk to download compiled project*/
+import iconic_apk from "./../../assets/programas/iconicMusic.apk";
 
 export function Mobile() {
+  const content = (
+    <div className="flex flex-col w-full h-full items-center bg-[var(--main-content-bg-color)]">
+      <div className="flex flex-col w-65 sm:w-100 items-start h-full">
+        <h2 className="text-lg font-medium text-[var(--text-color)]">
+          Proyectos de movil
+        </h2>
+        {Project({
+          title: "Iconic Music",
+          logos: [flutter],
+          imgs:[iconic1, iconic2],
+          url_github:"https://github.com/DANIElPEZ/iconic-music",
+          file:iconic_apk,
+          type_button:true
+        })}
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Nav_bar />
+      {content}
       <Footer />
     </>
   );
--- src/views/projects/web.jsx
+++ src/views/projects/web.jsx
@@ -1,10 +1,87 @@
 import { Nav_bar } from "./../../components/nav_bar.jsx";
 import { Footer } from "./../../components/footer.jsx";
+import { Project } from "../../components/project_section.jsx";
+import docker from "./../../assets/docker.png";
+import weather from "./../../assets/weather.png";
+/* web tecnologies */
+import reflex from "./../../assets/reflex.jpg";
+import html from './../../assets/html.webp'
+import css from './../../assets/css.png'
+import js from './../../assets/js.webp'
 
 export function Web() {
+
+  const content = (
+    <div className="flex flex-col w-full h-full items-center bg-[var(--main-content-bg-color)]">
+      <div className="flex flex-col w-65 sm:w-100 items-start h-full">
+        <h2 className="text-lg font-medium text-[var(--text-color)]">
+          Proyectos de web
+        </h2>
+        {Project({
+          title: "Docker Guide",
+          logos: [reflex],
+          imgs: [docker],
+          url_github: "https://github.com/DANIElPEZ/docker-guide",
+          url_web: "https://docker-guide.vercel.app/",
+          type_layout:true
+        })}
+
+        {Project({
+          title: "Weather for pilots",
+          logos:[html, css, js],
+          imgs: [weather],
+          url_github: "https://github.com/DANIElPEZ/weather-for-pilots",
+          url_web: "https://weather-for-pilots.vercel.app/",
+          type_layout:true
+        })}
+
+        <h2 className="text-lg font-medium text-[var(--text-color)]">
+          Otros proyectos
+        </h2>
+        {Project({
+          title: "Tetris Game",
+          logos:[html, css, js],
+          url_github: "https://github.com/DANIElPEZ/tetris-clone",
+          url_web: "https://tetris-clone-dnv.vercel.app/",
+        })}
+        {Project({
+          title: "Snake Game",
+          logos:[html, css, js],
+          url_github: "https://github.com/DANIElPEZ/snake-game-css-grid",
+          url_web: "https://snake-game-dnv.vercel.app/",
+        })}
+        {Project({
+          title: "Brick Breaker Game",
+          logos:[html, css, js],
+          url_github: "https://github.com/DANIElPEZ/brick-breaker-clone",
+          url_web: "https://brick-breaker-dnv.vercel.app/",
+        })}
+        {Project({
+          title: "Paint js",
+          logos:[html, css, js],
+          url_github: "https://github.com/DANIElPEZ/Paint-js",
+          url_web: "https://paint-js-dnv.vercel.app/",
+        })}
+        {Project({
+          title: "Tier Maker",
+          logos:[html, css, js],
+          url_github: "https://github.com/DANIElPEZ/tier-maker",
+          url_web: "https://tier-maker-dnv.vercel.app/",
+        })}
+        {Project({
+          title: "IA Privado",
+          logos:[html, css, js],
+          url_github: "https://github.com/DANIElPEZ/ia-privado",
+          url_web: "https://chatgpt-privado-dnv.vercel.app/",
+        })}
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Nav_bar />
+      {content}
       <Footer />
     </>
   );
